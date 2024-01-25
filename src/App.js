5213fa3948f664839a193c914f8fcc95fff15110
Naa-A <61292016+Naa-A@users.noreply.github.com>
2024-01-25 03:12:00 +0000

    Github link added

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ export default function App() {
     <div className="App">
       <h1>Weather App</h1>
       <Weather />
+      <footer>
+        This project is an
+        <a href="http://https://github.com/Naa-A/weather-app"></a>open sourced
+      </footer>
     </div>
   );
 }
